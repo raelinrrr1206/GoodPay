400d0bdbbc2e8a77f55feb066ad6df800e0c5919
RaeLin <raelinrrr1206@gmail.com>
2020-06-29 22:14:04 +0800

    Updates

---
+++ precache-manifest.48e1553c66d4c86ad133872d395035f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65684ad77ca863e8c0295f53f1f590c",
+    "url": "/GoodPay/index.html"
+  },
+  {
+    "revision": "d2220808733f9885b9cb",
+    "url": "/GoodPay/static/css/main.4314749f.chunk.css"
+  },
+  {
+    "revision": "8d535ea439dfcd0991be",
+    "url": "/GoodPay/static/js/2.7bbb78e7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/GoodPay/static/js/2.7bbb78e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2220808733f9885b9cb",
+    "url": "/GoodPay/static/js/main.0f054211.chunk.js"
+  },
+  {
+    "revision": "4eb13a81e62a03fb1448",
+    "url": "/GoodPay/static/js/runtime-main.e4149f97.js"
+  }
+]);
